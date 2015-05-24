cf2fcf832a20e362d1e7bc5f65e804daf3afac8e
Ernir <e.ernir@gmail.com>
2015-05-24 21:16:57 +0000

    The template from the old bufftracker copied over and Djangofied.

--- bufftracker/views.py
+++ bufftracker/views.py
@@ -1,3 +1,12 @@
+from bufftracker.models import Spell, Source
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    spell_list = Spell.objects.all()
+    source_list = Source.objects.all()
+
+    return render(request, "index.html", {
+        "spell_list": spell_list,
+        "source_list": source_list
+    })
