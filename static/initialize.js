1e3579289c7c9e0cc5dbb846b270a95e30a476c4
Ernir <e.ernir@gmail.com>
2015-05-25 17:06:03 +0000

    JS copied over from old bufftracker.

--- static/initialize.js
+++ static/initialize.js
@@ -1,3 +1,24 @@
+/*
+ Global variables.
+ (Hey, it's JS. Did you expect there wouldn't be any?
+ */
+var selectedSpellIDs;
+var statisticsGroups;
+var modifierTypes;
+
 $(function () {
-    console.log("You really should be seeing this.");
+    $.getJSON("/api/bufftracker/statistics/", function (data) {
+        if (data.status === 200) {
+            statisticsGroups = data.content;
+        }
+    });
+
+    $.getJSON("/api/bufftracker/modifiers/", function (data) {
+        if (data.status === 200) {
+            modifierTypes = data.content;
+        }
+    });
+    setListeners();
+
+    selectedSpellIDs = [];
 });
