6f42684b801fc69df4e6626bd81f0fdb2608dcbb
Ernir <e.ernir@gmail.com>
2015-06-03 10:18:50 +0000

    Spells can now be copied in the admin.

--- bufftracker/admin.py
+++ bufftracker/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from bufftracker import models
 from bufftracker.hardcoded_data import CasterLevelFormula
 
+
+class SpellAdmin(admin.ModelAdmin):
+    save_as = True
+
 admin.site.register(models.Source)
 admin.site.register(models.Statistic)
 admin.site.register(models.StatisticGroup)
@@ -9,5 +13,5 @@ admin.site.register(models.ModifierType)
 admin.site.register(models.NumericalBonus)
 admin.site.register(models.MiscBonus)
 admin.site.register(models.TempHPBonus)
-admin.site.register(models.Spell)
+admin.site.register(models.Spell, SpellAdmin)
 admin.site.register(CasterLevelFormula)
